8ed1cb0bd5d4a9f45fbda3fdd8f66ab053b7cc20
ASelviyan <angelikaselviyan5@gmail.com>
2022-07-26 10:55:21 -0600

    bugs

--- components/Main.jsx
+++ components/Main.jsx
@@ -13,7 +13,7 @@ export default function Main() {
                     <article className="se-content">
                         <div className='w-full text-center '>
                             <div>
-                                <Image src='/profilePic3.PNG' alt='/' width='260' height='240' className='rounded-full' />
+                                <Image src='profilePic3.PNG' alt='/' width='260' height='240' className='rounded-full' />
                                 <h2 className='mb-4'>Angelika Selviyan</h2>
                                 <p className='mt-6 font-medium BigFirstLetter'>I&apos;m a Software Developer with a creative mindset who believes that art and programming are intertwined. Both allow you to express your unique vision. The values I bring are collaboration through a friendly personal demeanor, an optimistic mindset, and a solution-focused mentality which comes from a strong drive to learn more about a problem and never give up on it. </p>
                             </div>
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -8,51 +8,51 @@ export default function Skills() {
             <h2>Skills</h2>
             <div className='flex flex-wrap items-center justify-center' id='skills'>
                 <div className='m-5'>
-                    <Image src='/portfolio/css.png' alt='/' width='180' height='180' className='m-4' />
+                    <Image src='portfolio/css.png' alt='/' width='180' height='180' className='m-4' />
                 </div>
 
                 <div className='m-5'>
-                    <Image src='/portfolio/html.png' alt='/' width='160' height='160' className='m-4' />
+                    <Image src='portfolio/html.png' alt='/' width='160' height='160' className='m-4' />
                 </div>
 
                 <div className='m-5'>
-                    <Image src='/portfolio/js.png' alt='/' width='170' height='190' className='mx-4 mb-2' />
+                    <Image src='portfolio/js.png' alt='/' width='170' height='190' className='mx-4 mb-2' />
                 </div>
 
                 <div className='m-5'>
-                    <Image src='/portfolio/sql.png' alt='/' width='140' height='140' className='m-4' />
+                    <Image src='portfolio/sql.png' alt='/' width='140' height='140' className='m-4' />
                 </div>
 
                 <div className='m-5'>
-                    <Image src='/portfolio/react.png' alt='/' width='140' height='140' className='m-4' />
+                    <Image src='portfolio/react.png' alt='/' width='140' height='140' className='m-4' />
                 </div>
 
                 <div className='m-5'>
-                    <Image src='/portfolio/next.png' alt='/' width='140' height='140' className='m-6' />
+                    <Image src='portfolio/next.png' alt='/' width='140' height='140' className='m-6' />
                 </div>
 
                 <div className='m-5'>
-                    <Image src='/portfolio/node.png' alt='/' width='170' height='90' className='m-4' />
+                    <Image src='portfolio/node.png' alt='/' width='170' height='90' className='m-4' />
                 </div>
 
                 <div className='m-5'>
-                    <Image src='/portfolio/mongo.png' alt='/' width='190' height='90' className='m-4' />
+                    <Image src='portfolio/mongo.png' alt='/' width='190' height='90' className='m-4' />
                 </div>
 
                 <div className='m-5'>
-                    <Image src='/portfolio/postgres.png' alt='/' width='230' height='50' className='m-4' />
+                    <Image src='portfolio/postgres.png' alt='/' width='230' height='50' className='m-4' />
                 </div>
 
                 <div className='m-5'>
-                    <Image src='/portfolio/bootstrap1.png' alt='/' width='260' height='100' className='m-4' />
+                    <Image src='portfolio/bootstrap1.png' alt='/' width='260' height='100' className='m-4' />
                 </div>
 
                 <div className='m-5'>
-                    <Image src='/portfolio/python.png' alt='/' width='190' height='60' className='ml-16' />
+                    <Image src='portfolio/python.png' alt='/' width='190' height='60' className='ml-16' />
                 </div>
 
                 <div className='m-5'>
-                    <Image src='/portfolio/tailwind.png' alt='/' width='230' height='40' className='m-4' />
+                    <Image src='portfolio/tailwind.png' alt='/' width='230' height='40' className='m-4' />
                 </div>
             </div>
         </>
