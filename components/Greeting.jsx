7ec82c10d8a82fe5cc16fef26b3ceb9f55342dea
ASelviyan <angelikaselviyan5@gmail.com>
2023-05-28 19:21:39 -0600

    bugs

--- components/Greeting.jsx
+++ components/Greeting.jsx
@@ -3,7 +3,7 @@ export default function Greeting(){
         <>
             <section className='lg:text-xl font-bold'><h2>Hello I&apos;m</h2></section>
             <h1 className="greetingAnimation ">Angelika Selviyan</h1>
-            <section className='lg:text-xl font-bold'><h2>Software Developer</h2></section> 
+            <section className='lg:text-xl font-bold'><h2>Software Developer!</h2></section> 
         </>
     )
 }
